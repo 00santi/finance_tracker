e498820d1bd2ca8340f506a2ced924cf30f2cf6a
Santi <santiagod.sddo@gmail.com>
2026-01-27 20:00:05 -0500

    small insert and retrieve test for database

--- src/db.rs
+++ src/db.rs
@@ -1,5 +1,6 @@
 use dotenvy::dotenv;
 use std::env;
+use argon2::PasswordHash;
 use sqlx::PgPool;
 
 pub async fn init_db_pool() -> Result<PgPool, super::DynError> {
@@ -7,4 +8,43 @@ pub async fn init_db_pool() -> Result<PgPool, super::DynError> {
     let url = env::var("DATABASE_URL")?;
     let pool = PgPool::connect(url.as_str()).await?;
     Ok(pool)
+}
+
+pub async fn clear_tables(pool: &PgPool) -> Result<(), sqlx::Error> {
+    sqlx::query("TRUNCATE TABLE transactions, users RESTART IDENTITY CASCADE")
+        .execute(pool)
+        .await?;
+    Ok(())
+}
+
+pub struct User {
+    pub id: usize,
+    pub name: Option<String>,
+    pub email: String,
+    pub password_hash: String,
+    pub creation_date: String,
+}
+
+impl User {
+    fn default() -> User {
+        User {
+            id: 0,
+            name: None,
+            email: String::new(),
+            password_hash: String::new(),
+            creation_date: String::new(),
+        }
+    }
+}
+
+struct Transaction {
+    id: usize,
+    user_id: usize,
+    category: String,
+    description: Option<String>,
+    amount: f64,
+}
+
+impl Transaction {
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,49 @@
-use actix_web::
-{
-    get,
-    post,
-    App,
-    HttpResponse,
-    HttpServer,
-    Responder
-};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, post};
 use sqlx;
 mod db;
+use db::{User, clear_tables, init_db_pool};
 type DynError = Box<dyn std::error::Error + Send + Sync>;
 
 #[actix_web::main]
 async fn main() -> Result<(), DynError> {
-    let pool = db::init_db_pool().await?;
-    sqlx::query("SELECT 1").execute(&pool).await?;
+    let db_pool = init_db_pool().await?;
+    let pool = &db_pool;
+    clear_tables(pool).await?;
 
-    return Ok(());
-    HttpServer::new(|| {
-        App::new()
-            .service(health)
-            .service(echo)
-    })
+    let email = "test@test.com".to_string();
+    let password_hash = "123".to_string();
+
+    sqlx::query!(
+        r#"
+        INSERT INTO users (email, password_hash)
+        VALUES ($1, $2)
+        "#,
+        email,
+        password_hash
+    ).execute(pool).await?;
+
+    let row = sqlx::query!(
+        r#"
+            SELECT id, name, email, password_hash, created_at
+            FROM users
+            WHERE email = $1
+        "#,
+        email
+    ).fetch_one(pool).await?;
+
+    println!(
+        "User id: {}, name: {:?}, email: {}, created_at: {:?}",
+        row.id, row.name, row.email, row.created_at
+    );
+
+    Ok(())
+
+    /*HttpServer::new(|| App::new().service(health).service(echo))
         .bind(("127.0.0.1", 7878))?
         .run()
         .await?;
 
-    Ok(())
+    Ok(())*/
 }
 
 #[get("/health")]
