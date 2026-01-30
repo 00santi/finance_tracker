ee1490ab68671e421b3aea890ab05f0b008c725b
Santi <santiagod.sddo@gmail.com>
2026-01-29 20:53:00 -0500

    removed unused structs

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,7 @@
 use dotenvy::dotenv;
 use std::env;
 use argon2::PasswordHash;
+use serde::Serialize;
 use sqlx::PgPool;
 
 pub async fn init_db_pool() -> Result<PgPool, super::DynError> {
@@ -15,36 +16,4 @@ pub async fn clear_tables(pool: &PgPool) -> Result<(), sqlx::Error> {
         .execute(pool)
         .await?;
     Ok(())
-}
-
-pub struct User {
-    pub id: usize,
-    pub name: Option<String>,
-    pub email: String,
-    pub password_hash: String,
-    pub created_at: String,
-}
-
-impl User {
-    fn default() -> User {
-        User {
-            id: 0,
-            name: None,
-            email: String::new(),
-            password_hash: String::new(),
-            created_at: String::new(),
-        }
-    }
-}
-
-struct Transaction {
-    id: usize,
-    user_id: usize,
-    category: String,
-    description: Option<String>,
-    amount: f64,
-}
-
-impl Transaction {
-
 }
--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,23 @@ use num_traits::cast::FromPrimitive;
 use serde::Deserialize;
 
 mod db;
-use db::{User, clear_tables, init_db_pool};
+use db::{clear_tables, init_db_pool};
 type DynError = Box<dyn std::error::Error + Send + Sync>;
 
 #[actix_web::main]
 async fn main() -> Result<(), DynError> {
     let pool = init_db_pool().await?;
+    let app_data = web::Data::new(pool);
 
-    HttpServer::new(|| App::new().service(health).service(echo))
-        .bind(("127.0.0.1", 7878))?
-        .run()
-        .await?;
+    HttpServer::new(move ||
+        App::new()
+            .app_data(app_data.clone())
+            .service(health)
+            .service(echo)
+            .service(create_user))
+            .bind(("127.0.0.1", 7878))?
+            .run()
+            .await?;
 
     Ok(())
 }
@@ -36,42 +42,22 @@ async fn echo(req_body: String) -> impl Responder {
 
 #[post("/users")]
 async fn create_user(pool: web::Data<PgPool>, req: web::Json<NewUserRequest>) -> impl Responder {
-    let pwd = make_hash(req.password.clone());
-    insert_new_user(req.username.clone().unwrap_or_default(), req.email.clone(), pwd, pool).await;
+    sqlx::query!(
+        r#"
+        INSERT INTO users (name, email, password_hash)
+        VALUES ($1, $2, $3)
+        "#,
+        req.username,
+        req.email,
+        make_hash(req.password.as_bytes()),
+    ).execute(pool.as_ref()).await.unwrap();
+
     HttpResponse::Ok().body("User added successfully")
 }
 
-async fn insert_new_user(name: String, email: String, password_hash: String, pool: web::Data<PgPool>) -> User {
-    sqlx::query!(
-        r#"
-        INSERT INTO users (email, password_hash)
-        VALUES ($1, $2)
-        "#,
-        email,
-        password_hash
-    ).execute(pool.get_ref()).await.unwrap();
-    
-    let row = sqlx::query!(
-        r#"
-            SELECT id, name, email, password_hash, created_at
-            FROM users
-            WHERE email = $1
-        "#,
-        email
-    ).fetch_one(pool.get_ref()).await.unwrap();
-    
-    User {
-        id: row.id as usize,
-        name: row.name,
-        email: row.email,
-        password_hash: row.password_hash,
-        created_at: String::from("placeholder_time"),
-    }
-}
-
-fn make_hash(pwd: String) -> String {
+fn make_hash(pwd: &[u8]) -> String {
     let salt = SaltString::generate(&mut OsRng);
-    Argon2::default().hash_password(pwd.as_bytes(), &salt).unwrap().to_string()
+    Argon2::default().hash_password(pwd, &salt).unwrap().to_string()
 }
 
 #[derive(Deserialize)]
@@ -80,62 +66,3 @@ struct NewUserRequest {
     email: String,
     password: String,
 }
-
-
-/*
-async fn test_insert_user(pool: &PgPool) {
-    let email = "test@test.com".to_string();
-    let password_hash = "123".to_string();
-
-    sqlx::query!(
-        r#"
-        INSERT INTO users (email, password_hash)
-        VALUES ($1, $2)
-        "#,
-        email,
-        password_hash
-    ).execute(pool).await.unwrap();
-
-    let row = sqlx::query!(
-        r#"
-            SELECT id, name, email, password_hash, created_at
-            FROM users
-            WHERE email = $1
-        "#,
-        email
-    ).fetch_one(pool).await.unwrap();
-
-    println!(
-        "User id: {}, name: {:?}, email: {}, created_at: {:?}",
-        row.id, row.name, row.email, row.created_at
-    );
-}
-
-async fn test_insert_trans(pool: &PgPool) {
-    let user_id = 1;
-    let amount = BigDecimal::from_f64(12.34).unwrap();
-    let category = "mmhmm";
-
-    sqlx::query!(
-        r#"
-        INSERT INTO transactions (user_id, amount, category)
-        VALUES ($1, $2, $3)
-        "#,
-        user_id,
-        amount,
-        category,
-    ).execute(pool).await.unwrap();
-
-    let row = sqlx::query!(
-        r#"
-            SELECT id, user_id, amount, category, description, created_at
-            FROM transactions
-            WHERE user_id = $1
-        "#,
-        user_id
-    ).fetch_one(pool).await.unwrap();
-
-    println!("Transaction id: {:?}, User id: {:?}, amount: {:?}, category: {:?}, description: {:?},  created at: {:?}",
-             row.id, row.user_id, row.amount, row.category, row.description, row.created_at);
-}
- */
