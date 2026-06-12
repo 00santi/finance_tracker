27ebdb75380029650d72526fb0ceb2626738f7cf
Santi <santiagod.sddo@gmail.com>
2026-06-12 17:23:43 -0400

    start of react frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+//import './App.css'
+
+function App() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Finance Tracker Frontend</h1>
+          <p>App goes here</p>
+        </header>
+      </div>
+  )
+}
+
+export default App;
